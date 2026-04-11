4f56999b591791224fde5312ca0ade34d093bb76
yotammadem <yotammadem@gmail.com>
2026-04-11 10:16:26 +0300

    implement tty module

--- src/tty/tty.c
+++ src/tty/tty.c
@@ -9,6 +9,9 @@
  * tests can define the target contract before kernel integration begins.
  */
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include "tty/tty.h"
 
 void tty_init(
@@ -25,30 +28,70 @@ void tty_init(
 }
 
 void tty_set_mode(struct tty *tty, enum tty_mode mode) {
-    (void)tty;
-    (void)mode;
+    tty->mode = mode;
 }
 
 enum tty_mode tty_get_mode(const struct tty *tty) {
-    (void)tty;
-    return TTY_MODE_COOKED;
+    return tty->mode;
 }
 
 char tty_read_raw_blocking(struct tty *tty) {
-    (void)tty;
-    return 0;
+    return (char)tty->read_input_byte_blocking();
+}
+
+static int tty_echo_bytes(struct tty *tty, const char *buf, int len) {
+    if (!tty->echo_enabled || tty->write_output == 0) {
+        return len;
+    }
+
+    return tty_write(tty, buf, len);
 }
 
 int tty_read(struct tty *tty, char *buf, int len) {
-    (void)tty;
-    (void)buf;
-    (void)len;
-    return 0;
+    int count;
+    uint8_t value;
+    static const char tty_backspace_erase[] = "\b \b";
+
+    if (len <= 0) {
+        return 0;
+    }
+
+    if (tty->mode == TTY_MODE_RAW) {
+        buf[0] = (char)tty->read_input_byte_blocking();
+        return 1;
+    }
+
+    count = 0;
+    for (;;) {
+        value = tty->read_input_byte_blocking();
+
+        if (value == '\b') {
+            if (count > 0) {
+                count--;
+                (void)tty_echo_bytes(tty, tty_backspace_erase, 3);
+            }
+            continue;
+        }
+
+        if (value == '\n') {
+            if (count < len) {
+                buf[count++] = '\n';
+            }
+            (void)tty_echo_bytes(tty, "\n", 1);
+            return count;
+        }
+
+        if (count < len) {
+            buf[count++] = (char)value;
+            (void)tty_echo_bytes(tty, (const char *)&buf[count - 1], 1);
+        }
+    }
 }
 
 int tty_write(struct tty *tty, const char *buf, int len) {
-    (void)tty;
-    (void)buf;
-    (void)len;
-    return 0;
+    if (tty->write_output == 0) {
+        return len;
+    }
+
+    return tty->write_output(tty->output_ctx, buf, len);
 }
